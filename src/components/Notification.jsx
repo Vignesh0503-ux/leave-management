c45266074f13c18d3e517a366626746b24040801
Yuvanesh M <yuvanesh0074@gmail.com>
2026-07-01 20:00:42 +0530

    Initial Commit

---
+++ src/components/Notification.jsx
@@ -0,0 +1,15 @@
+function Notification({
+  message,
+  type
+}) {
+
+  return (
+
+    <div className={`notification ${type}`} >
+      {message}
+    </div>
+
+  );
+}
+
+export default Notification;
